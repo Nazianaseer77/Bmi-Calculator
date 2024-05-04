0652d287d4347a38a5f32036019ab032cc0d461b
Nazianaseer77 <160523698+Nazianaseer77@users.noreply.github.com>
2024-05-04 22:46:57 +0500

    Bmi Calculator

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//#! /usr/bin/env node
+let weightInKg = 70; //70kg
+let heightInMeters = 1.75; //1.75m
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(`yourBMI is ${bmi}`);
+// let a: number = 5;
+// let b: number = 2;
+// let c: number;
+// c = ++a + --b + a + b++ +b;
+// console.log(c)
---
+++ app.ts
@@ -0,0 +1,18 @@
+//#! /usr/bin/env node
+let weightInKg = 70;  //70kg
+let heightInMeters =1.75;  //1.75m
+let bmi = weightInKg /(heightInMeters*heightInMeters);
+console.log(`yourBMI is ${bmi}`);
+
+
+
+
+
+
+
+
+// let a: number = 5;
+// let b: number = 2;
+// let c: number;
+// c = ++a + --b + a + b++ +b;
+// console.log(c)
